8b602679452ab77e826a7a172d3ba338634da313
Student <student@example.com>
2025-12-18 20:46:11 +0530

    Add root endpoint to server

--- apps/server/index.js
+++ apps/server/index.js
@@ -318,6 +318,10 @@ app.get("/health", (_req, res) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("NONO Smart Time Tracker API is running!");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
